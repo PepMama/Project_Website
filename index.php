8cf939035a4badb23bb39846bcbd559e568c417c
Smain <moussa59124@gmail.com>
2021-10-22 01:48:54 +0200

    Updating...

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+//* to access variable SESSION we need to:
+session_start();
+
+$_SESSION;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web site</title>
+</head>
+
+<body>
+    <a href="logout.php">Logout</a>
+    <h1>This is the index page</h1>
+
+    <br><br>
+    Hello, Username
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="styles/login.css">
+</head>
+
+
+<div id="box">
+    <form method="POST">
+        <div id="login">Login</div><br>
+
+        <input id="text" type="text" name="user_name" placeholder="Username"><br><br>
+        <input id="text" type="password" name="password" placeholder="Password"><br><br>
+
+        <input id="text" type="submit" value="Login"><br><br>
+
+        <a href="signup.php">Signup</a>
+
+    </form>
+
+</div>
+
+<body>
+
+</body>
+
+</html>
